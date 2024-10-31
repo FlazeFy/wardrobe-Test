0f17c262921eb72240ab43a72cc2117d2c94646b
Leonardho Rante S <newbie8801@gmail.com>
2024-10-31 23:34:22 +0700

    [TEST] Cypress init

---
+++ cypress.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+    e2e: {
+        // baseUrl : 'https://kumande.leonardhors.site',
+
+        // For Backend Apps
+        baseUrl: 'http://127.0.0.1:8000/',
+
+        // For Frontend Apps
+        // baseUrl: 'http://localhost:3001/',
+        
+        specPattern : "support",
+        supportFile : false,
+    }
+})
