beb648b5b9d68d4db13a44b989bd89c05ea74b48
Leonardho Rante S <flazen.edu@gmail.com>
2025-03-16 21:18:01 +0700

    [Integration TEST] API Soft Delete Clothes By Id, Hard Delete Clothes By Id, Hard Delete Wash By Id

--- support/integrations/clothes/delete_clothes.js
+++ support/integrations/clothes/delete_clothes.js
@@ -6,6 +6,22 @@ describe('Wardrobe API Testing - Clothes', () => {
     const method = 'delete'
     const token = generateAuthToken("hardcode")
 
+    it('Soft Delete Clothes By Id', () => {
+        const id = '17963858-9771-11ee-8f4a-32164291er4w' 
+        
+        cy.request({
+            method: method,
+            url: `/api/v1/clothes/delete/${id}`,
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).as('SoftDeleteClothesById')
+        cy.get('@SoftDeleteClothesById').then(dt => {
+            cy.templateDelete(dt)
+            expect(dt.body.message).contain('clothes deleted')
+        })
+    })
+
     it('Hard Delete Clothes By Id', () => {
         const id = '17963858-9771-11ee-8f4a-32164291er4w' 
         
@@ -19,6 +35,38 @@ describe('Wardrobe API Testing - Clothes', () => {
         cy.get('@HardDeleteClothesById').then(dt => {
             cy.templateDelete(dt)
             expect(dt.body.message).contain('clothes permentally deleted')
+        })
+    })
+
+    it('Hard Delete Schedule By Id', () => {
+        const id = '17963858-9771-11ee-8f4a-32164291er4w' 
+        
+        cy.request({
+            method: method,
+            url: `/api/v1/clothes/destroy_schedule/${id}`,
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).as('HardDeleteScheduleById')
+        cy.get('@HardDeleteScheduleById').then(dt => {
+            cy.templateDelete(dt)
+            expect(dt.body.message).contain('schedule permentally deleted')
+        })
+    })
+
+    it('Hard Delete Wash By Id', () => {
+        const id = '17963858-9771-11ee-8f4a-32164291er4w' 
+        
+        cy.request({
+            method: method,
+            url: `/api/v1/clothes/destroy_wash/${id}`,
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).as('HardDeleteWashById')
+        cy.get('@HardDeleteWashById').then(dt => {
+            cy.templateDelete(dt)
+            expect(dt.body.message).contain('clothes wash permentally deleted')
         })
     })
 })
