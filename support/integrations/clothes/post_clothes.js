b3abc7fc08d9f08ce10d79678d5335fd7b361d73
Leonardho Rante S <flazen.edu@gmail.com>
2025-03-18 13:58:29 +0700

    [Integration TEST] API Post Wash Clothes, Post Save Outfit, Post Clothes

--- support/integrations/clothes/post_clothes.js
+++ support/integrations/clothes/post_clothes.js
@@ -68,4 +68,103 @@ describe('Wardrobe API Testing - Clothes', () => {
             expect(dt.body.message).contain('outfit history created with')
         })
     })
+
+    it('Post Wash Clothes', () => {
+        const payload = {
+            clothes_id : '10bacb64-e819-11ed-a05b-0242ac120003',
+            wash_note : 'test',
+            wash_type : 'Laundry',
+            wash_checkpoint : {
+                id : 1,
+                checkpoint_name : "rendam",
+                is_finished : false
+            },
+        }
+        
+        cy.request({
+            method: method,
+            url: '/api/v1/clothes/wash',
+            body: payload,
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).as('PostWashClothes')
+        cy.get('@PostWashClothes').then(dt => {
+            cy.templatePost(dt,null)
+            expect(dt.body.message).contain('clothes wash history created')
+        })
+    })
+
+    it('Post Save Outfit', () => {
+        const payload = {
+            list_outfit : [
+                {
+                    data : [
+                        {
+                            id : 'efbf49d9-78f4-436a-07ef-ca3aa661f9d7',
+                            clothes_name : 'shirt A',
+                            clothes_category : 'head',
+                            clothes_type : 'hat',
+                            clothes_merk : 'Nike',
+                            clothes_made_from : 'cotton',
+                            clothes_color : 'blue',
+                            clothes_image : 'https://storage.googleapis.com',
+                            last_used : '2025-01-11 11:09:18',
+                            total_used : 2,
+                        },
+                    ],
+                    created_at : '2025-01-17T03:35:56.340Z',
+                    outfit_name : 'Outfit Generated 17-Jan-2025 10:35',
+                },
+            ]
+        }
+        
+        cy.request({
+            method: method,
+            url: '/api/v1/clothes/wash',
+            body: payload,
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).as('PostSaveOutfit')
+        cy.get('@PostSaveOutfit').then(dt => {
+            cy.templatePost(dt,null)
+            expect(dt.body.message).contain('clothes wash history created')
+        })
+    })
+
+    it('Post Clothes', () => {
+        const payload = {
+            clothes_name: 'Clothes A',
+            clothes_category: 'upper_body',
+            clothes_desc: 'A stylish casual outfit',
+            clothes_merk: 'BrandX',
+            clothes_color: 'blue',
+            clothes_price: 500000,
+            clothes_image: null,
+            clothes_size: 'M',
+            clothes_gender: 'unisex',
+            clothes_made_from: 'cotton',
+            clothes_type: 'shirt',
+            clothes_buy_at: '2024-07-15',
+            clothes_qty: 1,
+            is_faded: 0,
+            has_washed: 1,
+            has_ironed: 1,
+            is_favorite: 0
+        }
+        
+        cy.request({
+            method: method,
+            url: '/api/v1/clothes',
+            body: payload,
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).as('PostClothes')
+        cy.get('@PostClothes').then(dt => {
+            cy.templatePost(dt,null)
+            expect(dt.body.message).contain('clothes created, its called')
+        })
+    })
 })
