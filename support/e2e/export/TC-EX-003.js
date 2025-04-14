303abf528ace885884cb7dc46a9b3db88aba4ff8
Leonardho Rante S <flazen.edu@gmail.com>
2025-04-14 16:59:01 +0700

    [E2E TEST] TC-EX-003 - User Can Download Exported Data Of Clothes Used History

--- support/e2e/export/TC-EX-003.js
+++ support/e2e/export/TC-EX-003.js
@@ -1,36 +1,36 @@
 // Components
 import '../../components/template'
 
-describe('Wardrobe E2E Test - TC-EX-002 - Export', () => {
+describe('Wardrobe E2E Test - TC-EX-003 - Export', () => {
     const username = 'flazefy'
     const password = 'nopass123'
     const date = new Date().toISOString().replace(/:/g, '-')
 
-    it('User Can Download Exported Data Of Clothes', () => {
+    it('User Can Download Exported Data Of Clothes Used History', () => {
         // Pre Condition : User Must Logged In To Their Account
         cy.templateE2ELogin(username, password).then(() => {
             // Step 1: After Signed In. In the Navbar, Click the menu button that contain username to open the Profile Page
             cy.get('.navbar').contains('a', username).click()
             cy.url().should('include', '/profile')
 
-            // Step 2: At the "Export My Data" section, find the sub section "Clothes Data". Click button "Download Excel" to export clothes data
+            // Step 2: At the "Export My Data" section, find the sub section "Clothes Used History Data". Click button "Download Excel" to export clothes data
             cy.get('#export_data-section').should('exist').within(()=>{
                 cy.get('h2').contains('Export My Data')
-                cy.get('#export_data_clothes-section').should('exist').within(()=>{
-                    cy.get('h5').contains('Clothes Data')
+                cy.get('#export_data_clothes_used_history-section').should('exist').within(()=>{
+                    cy.get('h5').contains('Clothes Used History Data')
                     cy.contains('a','Download excel').click()
                 })
             })
 
-            // Step 3: Success pop up with text "Clothes data downloaded" will appear
+            // Step 3: Success pop up with text "Clothes Used data downloaded" will appear
             cy.get('.swal2-popup:not(.swal2-loading)', { timeout: 5000 }).should('exist').then(() => {
                 cy.get('.swal2-html-container').invoke('text').then((text)=>{
-                    expect(text).to.equal('Clothes data downloaded')
+                    expect(text).to.equal('Clothes Used data downloaded')
                 })
                 cy.get('.swal2-popup').contains('button', 'Okay!').click()
             })
             // Evidence - Step 3
-            cy.screenshot(`TC-EX-002_Step-3-${date}`)
+            cy.screenshot(`TC-EX-003_Step-3-${date}`)
         })
     })
 })
